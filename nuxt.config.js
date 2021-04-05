65ea5e4e5bdbb5d0aff49db7549da804ba456c14
Antonio <antoniops0505@gmail.com>
2021-04-05 12:24:10 +0200

    Subida produccion

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,8 @@ export default {
     {rel: "stylesheet", type: "text/css", href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"}],
   },
 
+  target: 'static',
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
